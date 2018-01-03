116501ebd588f3a976ae02cd2355dc8aa9821151
amir khanlari <amir.khanlari@gmail.com>
2018-01-03 17:46:59 +0330

    All Plugin

---
+++ src/Plugins/Nop.Plugin.Widgets.CarouselSlider/Components/WidgetsCarouselSliderViewComponent.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Nop.Core;
+using Nop.Core.Caching;
+using Nop.Plugin.Widgets.CarouselSlider.Infrastructure.Cache;
+using Nop.Plugin.Widgets.CarouselSlider.Models;
+using Nop.Services.Configuration;
+using Nop.Services.Media;
+using Nop.Web.Framework.Components;
+
+namespace Nop.Plugin.Widgets.CarouselSlider.Components
+{
+    [ViewComponent(Name = "WidgetsCarouselSlider")]
+    public class WidgetsCarouselSliderViewComponent : NopViewComponent
+    {
+        private readonly IStoreContext _storeContext;
+        private readonly IStaticCacheManager _cacheManager;
+        private readonly ISettingService _settingService;
+        private readonly IPictureService _pictureService;
+
+        public WidgetsCarouselSliderViewComponent(IStoreContext storeContext, 
+            IStaticCacheManager cacheManager, 
+            ISettingService settingService, 
+            IPictureService pictureService)
+        {
+            this._storeContext = storeContext;
+            this._cacheManager = cacheManager;
+            this._settingService = settingService;
+            this._pictureService = pictureService;
+        }
+
+        public IViewComponentResult Invoke(string widgetZone, object additionalData)
+        {
+            var CarouselSliderSettings = _settingService.LoadSetting<CarouselSliderSettings>(_storeContext.CurrentStore.Id);
+
+            var model = new PublicInfoModel
+            {
+                Picture1Url = GetPictureUrl(CarouselSliderSettings.Picture1Id),
+                Text1 = CarouselSliderSettings.Text1,
+                Link1 = CarouselSliderSettings.Link1,
+
+                Picture2Url = GetPictureUrl(CarouselSliderSettings.Picture2Id),
+                Text2 = CarouselSliderSettings.Text2,
+                Link2 = CarouselSliderSettings.Link2,
+
+                Picture3Url = GetPictureUrl(CarouselSliderSettings.Picture3Id),
+                Text3 = CarouselSliderSettings.Text3,
+                Link3 = CarouselSliderSettings.Link3,
+
+                Picture4Url = GetPictureUrl(CarouselSliderSettings.Picture4Id),
+                Text4 = CarouselSliderSettings.Text4,
+                Link4 = CarouselSliderSettings.Link4,
+
+                Picture5Url = GetPictureUrl(CarouselSliderSettings.Picture5Id),
+                Text5 = CarouselSliderSettings.Text5,
+                Link5 = CarouselSliderSettings.Link5
+            };
+
+            if (string.IsNullOrEmpty(model.Picture1Url) && string.IsNullOrEmpty(model.Picture2Url) &&
+                string.IsNullOrEmpty(model.Picture3Url) && string.IsNullOrEmpty(model.Picture4Url) &&
+                string.IsNullOrEmpty(model.Picture5Url))
+                //no pictures uploaded
+                return Content("");
+
+            return View("~/Plugins/Widgets.CarouselSlider/Views/PublicInfo.cshtml", model);
+        }
+
+        protected string GetPictureUrl(int pictureId)
+        {
+            var cacheKey = string.Format(ModelCacheEventConsumer.PICTURE_URL_MODEL_KEY, pictureId);
+            return _cacheManager.Get(cacheKey, () =>
+            {
+                //little hack here. nulls aren't cacheable so set it to ""
+                var url = _pictureService.GetPictureUrl(pictureId, showDefaultPicture: false) ?? "";
+                return url;
+            });
+        }
+    }
+}
---
+++ src/Plugins/Nop.Plugin.Widgets.CarouselSlider/Controllers/WidgetsCarouselSliderController.cs
@@ -0,0 +1,172 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Nop.Core;
+using Nop.Core.Caching;
+using Nop.Plugin.Widgets.CarouselSlider.Models;
+using Nop.Services.Configuration;
+using Nop.Services.Localization;
+using Nop.Services.Media;
+using Nop.Services.Security;
+using Nop.Services.Stores;
+using Nop.Web.Framework;
+using Nop.Web.Framework.Controllers;
+
+namespace Nop.Plugin.Widgets.CarouselSlider.Controllers
+{
+    [Area(AreaNames.Admin)]
+    public class WidgetsCarouselSliderController : BasePluginController
+    {
+        private readonly IWorkContext _workContext;
+        private readonly IStoreService _storeService;
+        private readonly IPermissionService _permissionService;
+        private readonly IPictureService _pictureService;
+        private readonly ISettingService _settingService;
+        private readonly ILocalizationService _localizationService;
+
+        public WidgetsCarouselSliderController(IWorkContext workContext,
+            IStoreService storeService,
+            IPermissionService permissionService, 
+            IPictureService pictureService,
+            ISettingService settingService,
+            ICacheManager cacheManager,
+            ILocalizationService localizationService)
+        {
+            this._workContext = workContext;
+            this._storeService = storeService;
+            this._permissionService = permissionService;
+            this._pictureService = pictureService;
+            this._settingService = settingService;
+            this._localizationService = localizationService;
+        }
+
+        public IActionResult Configure()
+        {
+            if (!_permissionService.Authorize(StandardPermissionProvider.ManageWidgets))
+                return AccessDeniedView();
+
+            //load settings for a chosen store scope
+            var storeScope = this.GetActiveStoreScopeConfiguration(_storeService, _workContext);
+            var CarouselSliderSettings = _settingService.LoadSetting<CarouselSliderSettings>(storeScope);
+            var model = new ConfigurationModel
+            {
+                Picture1Id = CarouselSliderSettings.Picture1Id,
+                Text1 = CarouselSliderSettings.Text1,
+                Link1 = CarouselSliderSettings.Link1,
+                Picture2Id = CarouselSliderSettings.Picture2Id,
+                Text2 = CarouselSliderSettings.Text2,
+                Link2 = CarouselSliderSettings.Link2,
+                Picture3Id = CarouselSliderSettings.Picture3Id,
+                Text3 = CarouselSliderSettings.Text3,
+                Link3 = CarouselSliderSettings.Link3,
+                Picture4Id = CarouselSliderSettings.Picture4Id,
+                Text4 = CarouselSliderSettings.Text4,
+                Link4 = CarouselSliderSettings.Link4,
+                Picture5Id = CarouselSliderSettings.Picture5Id,
+                Text5 = CarouselSliderSettings.Text5,
+                Link5 = CarouselSliderSettings.Link5,
+                ActiveStoreScopeConfiguration = storeScope
+            };
+
+            if (storeScope > 0)
+            {
+                model.Picture1Id_OverrideForStore = _settingService.SettingExists(CarouselSliderSettings, x => x.Picture1Id, storeScope);
+                model.Text1_OverrideForStore = _settingService.SettingExists(CarouselSliderSettings, x => x.Text1, storeScope);
+                model.Link1_OverrideForStore = _settingService.SettingExists(CarouselSliderSettings, x => x.Link1, storeScope);
+                model.Picture2Id_OverrideForStore = _settingService.SettingExists(CarouselSliderSettings, x => x.Picture2Id, storeScope);
+                model.Text2_OverrideForStore = _settingService.SettingExists(CarouselSliderSettings, x => x.Text2, storeScope);
+                model.Link2_OverrideForStore = _settingService.SettingExists(CarouselSliderSettings, x => x.Link2, storeScope);
+                model.Picture3Id_OverrideForStore = _settingService.SettingExists(CarouselSliderSettings, x => x.Picture3Id, storeScope);
+                model.Text3_OverrideForStore = _settingService.SettingExists(CarouselSliderSettings, x => x.Text3, storeScope);
+                model.Link3_OverrideForStore = _settingService.SettingExists(CarouselSliderSettings, x => x.Link3, storeScope);
+                model.Picture4Id_OverrideForStore = _settingService.SettingExists(CarouselSliderSettings, x => x.Picture4Id, storeScope);
+                model.Text4_OverrideForStore = _settingService.SettingExists(CarouselSliderSettings, x => x.Text4, storeScope);
+                model.Link4_OverrideForStore = _settingService.SettingExists(CarouselSliderSettings, x => x.Link4, storeScope);
+                model.Picture5Id_OverrideForStore = _settingService.SettingExists(CarouselSliderSettings, x => x.Picture5Id, storeScope);
+                model.Text5_OverrideForStore = _settingService.SettingExists(CarouselSliderSettings, x => x.Text5, storeScope);
+                model.Link5_OverrideForStore = _settingService.SettingExists(CarouselSliderSettings, x => x.Link5, storeScope);
+            }
+
+            return View("~/Plugins/Widgets.CarouselSlider/Views/Configure.cshtml", model);
+        }
+
+        [HttpPost]
+        public IActionResult Configure(ConfigurationModel model)
+        {
+            if (!_permissionService.Authorize(StandardPermissionProvider.ManageWidgets))
+                return AccessDeniedView();
+
+            //load settings for a chosen store scope
+            var storeScope = this.GetActiveStoreScopeConfiguration(_storeService, _workContext);
+            var CarouselSliderSettings = _settingService.LoadSetting<CarouselSliderSettings>(storeScope);
+
+            //get previous picture identifiers
+            var previousPictureIds = new[] 
+            {
+                CarouselSliderSettings.Picture1Id,
+                CarouselSliderSettings.Picture2Id,
+                CarouselSliderSettings.Picture3Id,
+                CarouselSliderSettings.Picture4Id,
+                CarouselSliderSettings.Picture5Id
+            };
+
+            CarouselSliderSettings.Picture1Id = model.Picture1Id;
+            CarouselSliderSettings.Text1 = model.Text1;
+            CarouselSliderSettings.Link1 = model.Link1;
+            CarouselSliderSettings.Picture2Id = model.Picture2Id;
+            CarouselSliderSettings.Text2 = model.Text2;
+            CarouselSliderSettings.Link2 = model.Link2;
+            CarouselSliderSettings.Picture3Id = model.Picture3Id;
+            CarouselSliderSettings.Text3 = model.Text3;
+            CarouselSliderSettings.Link3 = model.Link3;
+            CarouselSliderSettings.Picture4Id = model.Picture4Id;
+            CarouselSliderSettings.Text4 = model.Text4;
+            CarouselSliderSettings.Link4 = model.Link4;
+            CarouselSliderSettings.Picture5Id = model.Picture5Id;
+            CarouselSliderSettings.Text5 = model.Text5;
+            CarouselSliderSettings.Link5 = model.Link5;
+
+            /* We do not clear cache after each setting update.
+             * This behavior can increase performance because cached settings will not be cleared 
+             * and loaded from database after each update */
+            _settingService.SaveSettingOverridablePerStore(CarouselSliderSettings, x => x.Picture1Id, model.Picture1Id_OverrideForStore, storeScope, false);
+            _settingService.SaveSettingOverridablePerStore(CarouselSliderSettings, x => x.Text1, model.Text1_OverrideForStore, storeScope, false);
+            _settingService.SaveSettingOverridablePerStore(CarouselSliderSettings, x => x.Link1, model.Link1_OverrideForStore, storeScope, false);
+            _settingService.SaveSettingOverridablePerStore(CarouselSliderSettings, x => x.Picture2Id, model.Picture2Id_OverrideForStore, storeScope, false);
+            _settingService.SaveSettingOverridablePerStore(CarouselSliderSettings, x => x.Text2, model.Text2_OverrideForStore, storeScope, false);
+            _settingService.SaveSettingOverridablePerStore(CarouselSliderSettings, x => x.Link2, model.Link2_OverrideForStore, storeScope, false);
+            _settingService.SaveSettingOverridablePerStore(CarouselSliderSettings, x => x.Picture3Id, model.Picture3Id_OverrideForStore, storeScope, false);
+            _settingService.SaveSettingOverridablePerStore(CarouselSliderSettings, x => x.Text3, model.Text3_OverrideForStore, storeScope, false);
+            _settingService.SaveSettingOverridablePerStore(CarouselSliderSettings, x => x.Link3, model.Link3_OverrideForStore, storeScope, false);
+            _settingService.SaveSettingOverridablePerStore(CarouselSliderSettings, x => x.Picture4Id, model.Picture4Id_OverrideForStore, storeScope, false);
+            _settingService.SaveSettingOverridablePerStore(CarouselSliderSettings, x => x.Text4, model.Text4_OverrideForStore, storeScope, false);
+            _settingService.SaveSettingOverridablePerStore(CarouselSliderSettings, x => x.Link4, model.Link4_OverrideForStore, storeScope, false);
+            _settingService.SaveSettingOverridablePerStore(CarouselSliderSettings, x => x.Picture5Id, model.Picture5Id_OverrideForStore, storeScope, false);
+            _settingService.SaveSettingOverridablePerStore(CarouselSliderSettings, x => x.Text5, model.Text5_OverrideForStore, storeScope, false);
+            _settingService.SaveSettingOverridablePerStore(CarouselSliderSettings, x => x.Link5, model.Link5_OverrideForStore, storeScope, false);
+            
+            //now clear settings cache
+            _settingService.ClearCache();
+            
+            //get current picture identifiers
+            var currentPictureIds = new[]
+            {
+                CarouselSliderSettings.Picture1Id,
+                CarouselSliderSettings.Picture2Id,
+                CarouselSliderSettings.Picture3Id,
+                CarouselSliderSettings.Picture4Id,
+                CarouselSliderSettings.Picture5Id
+            };
+
+            //delete an old picture (if deleted or updated)
+            foreach (var pictureId in previousPictureIds.Except(currentPictureIds))
+            { 
+                var previousPicture = _pictureService.GetPictureById(pictureId);
+                if (previousPicture != null)
+                    _pictureService.DeletePicture(previousPicture);
+            }
+
+            SuccessNotification(_localizationService.GetResource("Admin.Plugins.Saved"));
+            return Configure();
+        }
+    }
+}
